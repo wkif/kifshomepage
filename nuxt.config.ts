7d61fcf3692cd2c14de3239d2de16274762e85fe
wkif <kif101001000@163.com>
2023-07-21 19:23:49 +0800

    feat: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { presetIcons, defineConfig } from "unocss";
 export default defineNuxtConfig({
+  alias: {
+    "@": "./"
+  },
   modules: ["@unocss/nuxt"],
   unocss: {
     uno: true,
     icons: true,
     attributify: true,
     shortcuts: {},
-    rules: [],
+    rules: [
+      ["shadow", {
+        "box-shadow":
+          "3.4px 3.4px 2.7px rgba(0, 0, 0, 0.05),8.7px 8.7px 6.9px rgba(0, 0, 0, 0.071),17.7px 17.7px 14.2px rgba(0, 0, 0, 0.089),36.5px 36.5px 29.2px rgba(0, 0, 0, 0.11), 100px 100px 80px rgba(0, 0, 0, 0.16)"
+      }],
+      ["shadow-sm", {
+        "box-shadow":
+          "3.4px 2.6px 17.2px rgba(0, 0, 0, 0.019),8.7px 6.6px 43.4px rgba(0, 0, 0, 0.027),17.7px 13.5px 88.6px rgba(0, 0, 0, 0.033),36.5px 27.7px 182.5px rgba(0, 0, 0, 0.041),100px 76px 500px rgba(0, 0, 0, 0.06)"
+      }]
+    ],
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="kifshomepage" class="p-t-3rem h-100vh">
+  <div id="kifshomepage" class="p-t-3rem">
     <div class="flex flex-justify-around flex-row w-30%">
       <div class="nav" v-for="nav in navData" :key="nav.id">
         <a :href="nav.link"> {{ nav.name }}</a>
@@ -9,8 +9,8 @@
     <div class="w-100% flex flex-justify-center">
       <img class="h-5rem m-t-6rem" src="../public/logo.png" alt="logo" />
     </div>
-    <div>
-      <div class="w-100% flex flex-justify-center m-t-3rem">
+    <div shadow p-b-20px>
+      <div class="w-100% flex flex-justify-center m-t-3rem items-center">
         <div class="h-2rem" @click="changeEnginList">
           <img
             :src="searchEngineIcon"
@@ -20,14 +20,18 @@
           />
         </div>
         <div
-          class="invite-code-panel w-20% flex flex-justify-center border-rd-1 bg p-3px"
+          class="invite-code-panel w-40% flex flex-justify-center items-center border-rd-1 p-3px"
         >
           <input
-            class="h-2rem border-none bg w-100%"
+            class="h-3rem border-none w-100% shake-horizontal"
             type="text"
+            outline-none
             v-model="searchValue"
             @keyup.enter="search"
+            @onkeydown="search"
             required
+            border-rd-1
+            shadow-sm
           />
           <div class="i-mdi-clear text-2xl h-2rem m-l-1rem" @click="clear" />
         </div>
@@ -36,9 +40,10 @@
           <div class="i-mdi-magnify text-2xl h-2rem m-l-1rem" />
         </div>
       </div>
+
       <div
         v-show="searchEngineListShow"
-        class="enginList flex flex-justify-center m-t-1rem"
+        class="enginList flex flex-justify-center m-t-1rem items-center bg-#e1e1e1"
       >
         <div v-for="item in searchEngine" :key="item.id" class="item">
           <img
@@ -51,9 +56,45 @@
         </div>
       </div>
     </div>
+    <!-- 下拉按钮 -->
+    <div class="flex flex-justify-center items-center">
+      <div
+        class="i-mdi-chevron-down text-2xl h-2rem m-l-1rem"
+        v-show="historyshow"
+        @click="historyshow = false"
+      />
+      <div
+        class="i-mdi-chevron-up text-2xl h-2rem m-l-1rem"
+        v-show="!historyshow"
+        @click="historyshow = true"
+      />
+    </div>
+    <div
+      v-show="historyshow"
+      class="history flex flex-col flex-justify-center m-t-1rem"
+    >
+      <div
+        v-for="(item, index) in history"
+        :key="item"
+        class="item flex flex-justify-center items-center border-rd-1 p-3px m-r-1rem"
+      >
+        <a
+          :href="searchEngine[searchEngineIndex].link + item"
+          target="_blank"
+          >{{ item }}</a
+        >
+        <!-- 删除 -->
+        <div
+          class="i-mdi-close text-2xl h-2rem m-l-5rem"
+          @click="remove(index)"
+        />
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
+import data from "./data.json";
+
 const navData = [
   {
     id: 0,
@@ -98,6 +139,7 @@ const searchEngine = [
   },
 ];
 const searchEngineIndex = ref(0);
+const historyshow = ref(false);
 let searchEngineIcon = searchEngine[searchEngineIndex.value].icon;
 const searchEngineListShow = ref(false);
 const changeEnginList = () => {
@@ -111,26 +153,30 @@ const changeEngin = (id: number) => {
 let searchValue = ref("");
 const search = () => {
   if (!searchValue.value) return;
+  if (history.value.length > 10) history.value.shift();
+  if (history.value.find((item) => item === searchValue.value)) {
+  } else {
+    history.value.push(searchValue.value);
+  }
   window.open(searchEngine[searchEngineIndex.value].link + searchValue.value);
 };
 const clear = () => {
   searchValue.value = "";
 };
+const history = ref<string[]>([]);
+const getHistory = () => {
+  const historyData = localStorage.getItem("history");
+  if (historyData) {
+    history.value = JSON.parse(historyData);
+  }
+};
+const remove = (index: number) => {
+  history.value.splice(index, 1);
+};
+onMounted(() => {
+  getHistory();
+});
+onUnmounted(() => {
+  localStorage.setItem("history", JSON.stringify(history.value));
+});
 </script>
-<style lang="less" scoped>
-a {
-  color: #c91c1c;
-  text-decoration: none;
-  font-size: 1rem;
-}
-a:hover {
-  color: #fff;
-}
-.bg {
-  background-color: #fbba2d;
-}
-.bg2 {
-  background-color: #f58463;
-}
-</style>
-<style src="./style/gOmboMm.less"></style>
