40f7733fc327ddd108cc9fe8b3f8a76f92525c7b
wkif <kif101001000@163.com>
2023-09-04 10:58:59 +0800

    feat: 新增描点和chatgpt

--- components/Chatgpt.vue
+++ components/Chatgpt.vue
@@ -3,19 +3,11 @@ const show = ref(false);
 </script>
 
 <template>
-  <div fixed right-100px bottom-300px>
-    <div
-      class="i-mdi-scoreboard-outline"
-      border-radius-50%
-      text-2rem
-      box-shadow="0 0 10px #000"
-      cursor-pointer
-      @click="show = !show"
-    ></div>
+  <div fixed right-100px bottom-300px flex items-end>
     <div
       v-show="show"
       min-h-40vh
-      w-40vw
+      w-30vw
       rounded-10px
       p-10px
       class="text-gray-800 dark:bg-#32383f bg-#eaeef2 dark:text-gray-100 shadow swing-in-top-fwd"
@@ -29,9 +21,17 @@ const show = ref(false);
         allowfullscreen="true"
         webkitallowfullscreen="true"
         mozallowfullscreen="true"
-        src="https://chat.openai.com/"
+        src="https://chat-gpt-next-web-kif.vercel.app/"
       ></iframe>
     </div>
+    <div
+      class="i-mdi-scoreboard-outline"
+      border-radius-50%
+      text-2rem
+      box-shadow="0 0 10px #000"
+      cursor-pointer
+      @click="show = !show"
+    ></div>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,12 @@ const getStyle = () => {
 };
 
 const webList = ref<WebType[]>(data);
-
+const nameList = ref<string[]>([]);
+const getNameList = () => {
+  webList.value.forEach((item) => {
+    item.list?.length && nameList.value.push(item.name);
+  });
+};
 const open = (link: string) => {
   window.open(link);
 };
@@ -39,6 +44,7 @@ const init = () => {
 
 onMounted(() => {
   getStyle();
+  getNameList();
   // init();
 });
 </script>
@@ -46,75 +52,89 @@ onMounted(() => {
 <template>
   <div id="kifshomepage">
     <TopBox />
-    <div m-l-7vw m-r-7vw m-t-2vh overflow-auto id="content">
-      <div v-for="item in webList" :key="item.type">
-        <div v-if="item.list?.length">
-          <div text-20px p-10px flex text="#8c959f" class="tracking-in-expand">
-            {{ item.name }}
+    <div flex>
+      <div flex flex-col p-l-50px m-t-6vh>
+        <a v-for="item in nameList" :key="item" :href="'#' + item" p-8px>{{
+          item
+        }}</a>
+      </div>
+      <div m-l-7vw m-r-7vw m-t-2vh overflow-auto id="content">
+        <div v-for="item in webList" :key="item.type">
+          <div v-if="item.list?.length">
+            <div
+              text-20px
+              p-10px
+              flex
+              text="#8c959f"
+              :id="item.name"
+              class="tracking-in-expand"
+            >
+              {{ item.name }}
+            </div>
+            <div w-130px h-2px m-b-20px bg="#d7dadd"></div>
           </div>
-          <div w-130px h-2px m-b-20px bg="#d7dadd"></div>
-        </div>
 
-        <div flex flex-wrap>
-          <div
-            v-for="web in item.list"
-            :key="web.name"
-            m-20px
-            p-20px
-            rounded-10px
-            w-200px
-            cursor-pointer
-            class="text-gray-800 dark:bg-#32383f bg-#eaeef2 dark:text-gray-100 slide-in-top"
-            flex
-            flex-col
-            justify-between
-          >
-            <div flex items-center justify-end>
-              <div
-                v-for="tag in web.tags"
-                :key="tag"
-                text-10px
-                p-4px
-                m-4px
-                bg="#a475f9"
-                rounded-7px
-              >
-                {{ tag }}
+          <div flex flex-wrap>
+            <div
+              v-for="web in item.list"
+              :key="web.name"
+              m-20px
+              p-20px
+              rounded-10px
+              w-200px
+              cursor-pointer
+              class="text-gray-800 dark:bg-#32383f bg-#eaeef2 dark:text-gray-100 slide-in-top"
+              flex
+              flex-col
+              justify-between
+            >
+              <div flex items-center justify-end>
+                <div
+                  v-for="tag in web.tags"
+                  :key="tag"
+                  text-10px
+                  p-4px
+                  m-4px
+                  bg="#a475f9"
+                  rounded-7px
+                >
+                  {{ tag }}
+                </div>
               </div>
-            </div>
-            <div w="100%" h-1px m-1px bg="#a475f9"></div>
-            <!-- title -->
-            <div text-20px p-t-10px class="tracking-in-expand">
-              {{ web.name }}
-            </div>
-            <!-- desc -->
-            <div text-14px p-t-10px>{{ web.desc }}</div>
-            <div w="100%" h-1px m-t-2px bg="#a475f9"></div>
-            <div flex items-center justify-around>
-              <!-- home图标 -->
-              <div
-                v-if="web.home"
-                class="i-mdi-home"
-                text-2xl
-                h-2rem
-                m-1px
-                @click="open(web.home)"
-              ></div>
-              <!-- github图标 -->
-              <div
-                v-if="web.github"
-                class="i-mdi-github"
-                text-2xl
-                h-2rem
-                m-1px
-                @click="open(web.github)"
-              ></div>
+              <div w="100%" h-1px m-1px bg="#a475f9"></div>
+              <!-- title -->
+              <div text-20px p-t-10px class="tracking-in-expand">
+                {{ web.name }}
+              </div>
+              <!-- desc -->
+              <div text-14px p-t-10px>{{ web.desc }}</div>
+              <div w="100%" h-1px m-t-2px bg="#a475f9"></div>
+              <div flex items-center justify-around>
+                <!-- home图标 -->
+                <div
+                  v-if="web.home"
+                  class="i-mdi-home"
+                  text-2xl
+                  h-2rem
+                  m-1px
+                  @click="open(web.home)"
+                ></div>
+                <!-- github图标 -->
+                <div
+                  v-if="web.github"
+                  class="i-mdi-github"
+                  text-2xl
+                  h-2rem
+                  m-1px
+                  @click="open(web.github)"
+                ></div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <!-- <Chatgpt /> -->
+    <Chatgpt />
     <User />
     <News />
     <Footer />
