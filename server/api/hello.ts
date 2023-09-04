a2d51e1c5b8ecccd30a7447c92034e1552605315
wkif <kif101001000@163.com>
2023-09-04 15:46:03 +0800

    feat: 优化头图

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,10 +1,21 @@
 export default defineEventHandler((event) => {
-  // 1-131随机数
-  const random = Math.floor(Math.random() * 131 + 1)
-  const url = "https://kifimg.oss-cn-beijing.aliyuncs.com/bg/kif_bg_" + random + ".jpg"
+  const hour = new Date().getHours();
+  let length: number = 0;
+  let type = '';
+  if (hour >= 18 || hour <= 6) {
+    length = 109;
+    type = 'night';
+  } else {
+    length = 184;
+    type = 'light';
+  }
+
+  // 1-184随机数
+  const random = Math.floor(Math.random() * length + 1);
+  const url = `https://kifimg.oss-cn-beijing.aliyuncs.com/bg/${type}/kif-bg-${type}-${random}.jpg`;
   return {
     code: 0,
-    message: 'ok!',
-    data: url
-  }
-})
+    message: "ok!",
+    data: url,
+  };
+});
